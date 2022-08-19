56e62598a73466847b4b379bf34859a2395a393d
Alex <s.bosov2012@yandex.ru>
2022-08-19 11:33:22 +0300

    feat: add buttons for settings and add design for settings account

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,7 +1,87 @@
 <template>
-<Navbar>
+  <div>
+    <Navbar/>
+    <v-container>
+      <v-card-title>Настройки</v-card-title>
+      <v-row align-content="center">
+        <v-col
+            cols="1"
+            sm="2"
+            md="3"
+        >
+          <v-btn class="blue darken-1 white--text"
+          >
+            Настройка аккаунта
+          </v-btn>
+        </v-col>
+        <v-col
+            cols="2"
+            sm="2"
+            md="3"
+        >
+          <v-btn class="blue darken-1 white--text"
+          >
+            Изменение пароля
+          </v-btn>
+        </v-col>
+      </v-row>
 
-</Navbar>
+      <v-row align-content="center">
+        <v-col
+            cols="2"
+            sm="2"
+            md="3"
+        >
+          <v-btn class="blue darken-1 white--text"
+          >
+            Настройка компании
+          </v-btn>
+        </v-col>
+        <v-col
+            cols="2"
+            sm="2"
+            md="3"
+        >
+          <v-btn class="blue darken-1 white--text"
+          >
+            Настройка карточки
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+            cols="3"
+            sm="6"
+            md="12"
+        >
+          <v-card
+              max-width="500px"
+          >
+            <v-card-title>Настройки аккаунта</v-card-title>
+            <v-text-field
+                style="padding-left: 14px; padding-right: 14px"
+                solo
+                placeholder="Почта"
+
+            />
+            <v-text-field
+                style="padding-left: 14px; padding-right: 14px"
+                solo
+                placeholder="Номер телефона"
+            />
+
+            <v-card-actions>
+
+              <v-btn style="background-color: #63C7B2; margin-left: 6px">Сохранить</v-btn>
+
+            </v-card-actions>
+
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+  </div>
 </template>
 
 <script>
--- src/components/statistics/Statistics.vue
+++ src/components/statistics/Statistics.vue
@@ -1,15 +1,93 @@
 <template>
-  <Navbar>
+  <div>
+    <Navbar/>
+    <v-card
+        class="mx-auto text-center"
+        color="#1E88E5"
+        dark
+        max-width="150vh"
+        min-height="75vh"
 
-  </Navbar>
+    >
+      <v-card-text><h1>Новые клиенты</h1></v-card-text>
+
+      <v-card-text>
+        <v-sheet color="rgba(0, 0, 0, .12)" elevation="5" rounded>
+          <v-sparkline
+              :labels="labels"
+              :value="value"
+               color="white"
+              :gradient="gradient"
+              :smooth="radius || false"
+              :padding="padding"
+              :line-width="width"
+              :stroke-linecap="lineCap"
+              :gradient-direction="gradientDirection"
+              :fill="fill"
+              :type="type"
+              :auto-line-width="autoLineWidth"
+              :show-labels="true"
+              auto-draw
+              padding="12"
+          >
+          </v-sparkline>
+        </v-sheet>
+      </v-card-text>
+
+      <v-card-text>
+        <div class="text-h4 font-weight-thin">
+          Статистика за последнюю неделю
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+const gradients = [
+  ['#222'],
+  ['#42b3f4'],
+  ['red', 'orange', 'yellow'],
+  ['purple', 'violet'],
+  ['#00c6ff', '#F0F', '#FF0'],
+  ['#f72047', '#ffd200', '#1feaea'],
+]
+
 import Navbar from "@/components/navbar/Navbar";
 
 export default {
   name: "Statistics",
-  components: {Navbar}
+  components: {Navbar},
+  data: () => ({
+    labels: [
+      '18.08',
+      '19.08',
+      '20.08',
+      '21.08',
+      '22.08',
+      '23.08',
+      '24.08',
+    ],
+    value: [
+      15,
+      20,
+      3,
+      14,
+      5,
+      20,
+      26,
+    ],
+    width: 3,
+    radius: 10,
+    padding: 2,
+    lineCap: 'round',
+    gradient: gradients[5],
+    gradientDirection: 'top',
+    gradients,
+    fill: false,
+    type: 'trend',
+    autoLineWidth: true,
+  }),
 }
 </script>
 
